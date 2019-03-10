1ccf41fcb2f36b520c88f21c7c148bd71452e2b5
arnaud ferrand <arferrand@gmail.com>
2019-03-10 13:50:00 +0000

    adding static-storybook

---
+++ storybook-static/main.667b3a7e7e27af4570e0.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,p){p(397),p(474),n.exports=p(856)},474:function(n,o,p){"use strict";p.r(o);p(475),p(698),p(701)}},[[0,1,2]]]);
